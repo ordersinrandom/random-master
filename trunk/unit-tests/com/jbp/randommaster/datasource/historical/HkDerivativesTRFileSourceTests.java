86bfac672ae5fa3cfb3834a587ede8390002bade
orders.in.random@gmail.com <orders.in.random@gmail.com@eaedbeed-ef13-d5cc-0181-99e94f219d26>
2013-06-22 06:01:04 +0000

    Added unit test for AutoCloseable

--- trunk/unit-tests/com/jbp/randommaster/datasource/historical/HkDerivativesTRFileSourceTests.java
+++ trunk/unit-tests/com/jbp/randommaster/datasource/historical/HkDerivativesTRFileSourceTests.java
@@ -2,6 +2,7 @@ package com.jbp.randommaster.datasource.historical;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,7 +14,6 @@ import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class HkDerivativesTRFileSourceTests extends TestCase {
-	
 	
 	@Test
 	public void testReadLinesNoFiltering() {
@@ -96,4 +96,54 @@ public class HkDerivativesTRFileSourceTests extends TestCase {
 	}
 	
 
+	@Test
+	public void testCleanUp() throws IOException {
+		StringBuilder buf=new StringBuilder(1000);
+		buf.append("HHI,F,1211,0,,20121031,161458,10607,1,001\n");
+		buf.append("HHI,F,1211,0,,20121031,161458,10608,1,001\n");
+		buf.append("HHI,F,1211,0,,20121031,161458,10609,1,001\n");
+		buf.append("HHI,O,1211,8400,P,20121031,161459,2,5,001\n");
+		buf.append("MHI,O,1211,22400,C,20121031,161459,110,1,001");
+		
+		File tempFile=File.createTempFile("HkexTRFileSourceTests2", null);
+		tempFile.deleteOnExit();
+		FileWriter fw=new FileWriter(tempFile);
+		fw.write(buf.toString());
+		fw.close();
+		
+		int breakAtIndex = 3;
+		int currentIndex = 0;
+
+		// try sudden break out from the loop
+		try (HkDerivativesTRFileSource src=new HkDerivativesTRFileSource(tempFile.getAbsolutePath())) {
+			Iterable<HkDerivativesTR> data=src.getData();
+
+			for (@SuppressWarnings("unused") HkDerivativesTR d : data) {
+				Assert.assertEquals("Inside loop not requires cleanup. it is in error state.", true, src.requiresCleanUp());
+				
+				if (breakAtIndex==currentIndex) {
+					break;
+				}
+				
+				currentIndex++;
+			}
+			
+			Assert.assertEquals("Loop breaking not requires cleanup. it is in error state.", true, src.requiresCleanUp());
+		}
+		
+		// try no breaking from the loop
+		try (HkDerivativesTRFileSource src=new HkDerivativesTRFileSource(tempFile.getAbsolutePath())) {
+			Iterable<HkDerivativesTR> data=src.getData();
+
+			for (@SuppressWarnings("unused") HkDerivativesTR d : data) {
+				Assert.assertEquals("Inside loop not requires cleanup. it is in error state.", true, src.requiresCleanUp());
+				
+			}
+			
+			Assert.assertEquals("No loop breaking but requires cleanup. it is in error state.", false, src.requiresCleanUp());
+		}		
+		
+	}
+	
+	
 }
